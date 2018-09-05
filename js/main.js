a95dc7f90ecb6f60a13f9d4038107e35dd5f74e9
Licio Lentimo <lentimo@gmail.com>
2018-09-05 21:37:24 +0300

    Added smooth scrolling

--- js/main.js
+++ js/main.js
@@ -20,3 +20,21 @@ $(function() {
   });
   $('.open-menu').height($(window).height());
 });
+
+/*Smooth scrolling*/
+$('.cf a').on('click',function(event){
+    if(this.hash !== ''){
+        event.preventDefault();
+        const hash = this.hash;
+
+        $('html, body').animate(
+            {
+                scrollTop: $(hash).offset().top
+            },
+            800,
+            function(){
+                window.location.hash = hash;
+            }
+        );
+    }
+})
